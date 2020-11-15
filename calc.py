023e675d83e6d6e9ffe16e876999527903a3209a
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2020-11-15 16:58:19 +0300

    Add files via upload
    made by Fariza

---
+++ calc.py
@@ -0,0 +1,25 @@
+print("                 ")
+print("Добро пожаловать на мою первую програму")                        
+print("                 ")
+print("Ноль в качестве знака операции"
+      "\nзавершит работу программы")
+while True:
+    s = input("Знак (+,-,*,/): ")
+    if s == '0':
+        break
+    if s in ('+', '-', '*', '/'):
+        x = float(input("x="))
+        y = float(input("y="))
+        if s == '+':
+            print("%.2f" % (x+y))
+        elif s == '-':
+            print("%.2f" % (x-y))
+        elif s == '*':
+            print("%.2f" % (x*y))
+        elif s == '/':
+            if y != 0:
+                print("%.2f" % (x/y))
+            else:
+                print("Деление на ноль!")
+    else:
+        print("Неверный знак операции!")
