6d6cccdd1eaf4f9ac8960a747b52e6c2b680531c
ZafarHamidov <50060143+ZafarHamidov@users.noreply.github.com>
2021-01-07 15:44:21 +0300

    1LESSON1

---
+++ 1/1.py
@@ -0,0 +1,18 @@
+"""from turtle import*
+fd(100)
+lt(90)
+fd(100)
+lt(90)
+fd(100)
+lt(90)
+fd(100)
+lt(90)
+
+exitonclick()
+"""
+from turtle import*
+fd(60)
+bk(10)
+left(90)
+fd(50)
+exitonclick()
